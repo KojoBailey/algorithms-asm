f71802f3e53bb64312e1b205775be00911b357fa
Kojo Bailey <kojomolojo@gmail.com>
2026-02-07 22:03:12 +0000

    Remove unnecessary comments

--- find_max.asm
+++ find_max.asm
@@ -7,14 +7,14 @@ find_max:
 
 	mov eax, [rdx]
 	dec ecx
-	jz  return; if edx == 0
+	jz  return
 
 compare:
 	add   rdx, 4; move to next array element
 	mov   r8d, [rdx]
 	cmp   r8d, eax
-	cmovg eax, r8d; if r8d > eax
-	loop  compare; if ecx != 0
+	cmovg eax, r8d
+	loop  compare
 
 return:
 	ret
